edf33d9dc16701fd4212fb692c53a7faa45880c9
SantanaC4 <eduardo.sanati@gmail.com>
2022-05-06 03:40:48 -0300

    webpack5, Jest, SCSS, and ESLint

--- docs/Eduardo_Santana/modulo_3/webpack.common.js
+++ docs/Eduardo_Santana/modulo_3/webpack.common.js
@@ -1,4 +1,10 @@
 const path = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   entry: './src/index.ts',
@@ -12,10 +18,56 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: []
+    rules: [
+    {
+      test: /\.tsx?$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          // disable type checker - we will use it in fork plugin
+          transpileOnly: true
+        }
+      }
+    },
+    {
+      test: /\.(scss|css)$/,
+      use: [
+        process.env.NODE_ENV !== 'production'
+          ? 'style-loader'
+          : MiniCssExtractPlugin.loader,
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true
+          }
+        }
+      ]
+    }
+  ]
   },
   resolve: {
     extensions: ['.tsx', '.ts', '.js']
   },
-  plugins: []
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].[contenthash].css',
+      chunkFilename: '[id].css'
+    }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }]
+    })
+    new ESLintPlugin({
+      extensions: ['.tsx', '.ts', '.js'],
+      exclude: 'node_modules'
+   })
+  ]
 };
