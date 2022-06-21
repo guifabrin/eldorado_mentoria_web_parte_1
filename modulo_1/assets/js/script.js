0a73317fc164fcf3ba177c7e44f3ff9440586db0
Guilherme Moreira <gf.moreira@hotmail.com.br>
2022-06-21 07:59:39 -0300

    - Removendo comentários - Renomeando classes - Removendo stylesheets não usados - Inclusão de description. - Alteração de nomenclatura de classes - Otimização de códigos CSS - Alteração de responsividade do avatar, de px para % - Separação das fetchs - Alteração de forEach para map - Correção de momenclaturas de classes e variaveis

--- modulo_1/assets/js/script.js
+++ modulo_1/assets/js/script.js
@@ -1,63 +1,56 @@
-const search__id = document.querySelector('#search__id');
-const profileResult = document.querySelector('.inf');
-const profileRepos = document.querySelector('.rep');
+const searchId = document.querySelector('#searchId');
+const profileResult = document.querySelector('.information');
+const profileRepos = document.querySelector('.repositories');
 const url = "https://api.github.com/users";
 
-async function getUser(user) {
-    const profileResponse = await fetch(`${url}/${user}`);
-    const reposResponse = await  fetch(`${url}/${user}/repos`);
+async function getProfileUser(user) {
+    return fetch(`${url}/${user}`).then(response => response.json()
+    )
+}
 
-    const profileResult = await profileResponse.json();
-    const profileRepos = await reposResponse.json();
-
-    return {profileResult, profileRepos};
+async function getUserRepos(user) {
+    return fetch(`${url}/${user}/repos`).then(response => response.json()
+    )
 }
 
 function showProfile(user) {
     profileResult.innerHTML = `
-<section class="srcInf">
-<img class="srcInf__image" src="${user.avatar_url}" alt="">
-<ul class="srcInf__information">
-    <li class="srcInf__information__userName ">  ${user.name}</li>
-    <li class="srcInf__information__userLogin">  ${user.login}</li>
-    <li class="srcInf__information__userFollow"> Seguidores ${user.followers} | Seguindo ${user.following}</li>
-    <li class="srcInf__information__userCompany">  ${user.company}</li>
-    <li class="srcInf__information__userLocation">  ${user.location}</li>
-</ul>
-<div class="card-body">
-<a href="${user.html_url}" class="btn btn-warning btn-block">Ver Perfil</a>
-</div>
-</section>`;
+        <section class="personalInformation">
+            <section>
+                <img src="${user.avatar_url}" alt="">
+            </section>
+            <ul class="personalInformationList">
+                <li class="userName hidden">  ${user.name}</li>
+                <li class="userLogin">  ${user.login}</li>
+                <li class="userFollow hidden"> Seguidores ${user.followers} | Seguindo ${user.following}</li>
+                <li class="userCompany hidden">  ${user.company}</li>
+                <li class="userLocation hidden">  ${user.location}</li>
+            </ul>
+            <div class="btn">
+                <a href="${user.html_url}" target="_blank" class="btn btn-warning btn-block">Ver Perfil</a>
+            </div>
+        </section>`;
 }
 
-function showRepos(repos){
-    let output = '';
-
-    repos.forEach(repo =>{
-        output += `
-        <section class="repo">
-        <span><a target="_blank" class="repo__title"
-            href="${repo.html_url}"><strong>${repo.name}</strong></a></span><br>
-        <span class="repo__lastUpdate">Ultima Atualização: ${Intl.DateTimeFormat('pt-BR').format(new Date(repo.updated_at))}</span>
-        <span class="badge badge-primary">stars: ${repo.stargazers_count}</span>
-        <span class="badge badge-secondary">watch: ${repo.watchers_count}</span>
-        <span class="badge badge-success">forks ${repo.forks_count}</span>
-    </section> `
-    })
-
+function showRepos(repos) {
+    let output = repos.map(repo => {
+        return `
+            <section class="repositoriesInformation">
+                <span><a target="_blank" class="repositoriesTitle" href="${repo.html_url}"><strong>${repo.name}</strong></a></span><br>
+                <span class="repositoriesLastUpdate">Ultima Atualização: ${Intl.DateTimeFormat('pt-BR').format(new Date(repo.updated_at))}</span></br>
+                <span class="badge badgePrimary">stars: ${repo.stargazers_count}</span>
+                <span class="badge badgeSecondary">watch: ${repo.watchers_count}</span>
+                <span class="badge badgeSuccess">forks ${repo.forks}</span>
+            </section> `
+    }).join('')
     profileRepos.innerHTML = output;
-
 }
 
-search__id.addEventListener("keyup", (e) => {
-    const user = e.target.value;
+searchId.addEventListener("keypress", (event) => {
+    const user = event.target.value;
 
-    if (user.length > 0) {
-        getUser(user).then(res => {
-            showProfile(res.profileResult);
-            showRepos(res.profileRepos);
-        })
+    if (event.keyCode == 13) {
+        getProfileUser(user).then(showProfile).catch((error) => mostrarError());
+        getUserRepos(user).then(showRepos).catch((error) => mostrarError());
     }
-
-
 })
