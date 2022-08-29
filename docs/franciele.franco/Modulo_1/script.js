799149add054063ba48bd0612a532860866de13d
franciele Franco <franciele.alves1108@gmail.com>
2022-08-29 15:39:59 -0300

    atualizações

--- docs/franciele.franco/Modulo_1/script.js
+++ docs/franciele.franco/Modulo_1/script.js
@@ -1,68 +1,79 @@
-
-//declaração de variaveis
-  const usuario = document.querySelector('input#usuario')
-
-  usuario.addEventListener("keydown", function (evento) 
-{  const ENTER_KEY_CODE = 13
-  if (evento.keycode === ENTER_KEY_CODE)
-    { 
-        pesquisar()
-    }
-})
-
-  function pesquisar()
+function pesquisar() 
 {
-  url = `https://api.github.com/users/${usuario.value}/repos`
-    
-  if(usuario.value)
-    { 
-      return getApiGitHub()
-    }
+  const procurar = document.getElementById("usuario")
+  const perfil = document.getElementById("perfil")
+  const url = "https://api.github.com/users"
+  const cliente_id = "Iv1.f64f0476d5d303dd"
+  const cliente_secreto = "e3e98fc69544d6b5173169f3734909907a0a3ea2"    
+  const contar = 10
+  const ordenar = "created: ascs"
 
-  else
-   { 
-    alert('Por favor, digite algo!')
-   }
-}     
+  async function obterUsuario(user) {
+      const perfilResposta = await fetch
+      (
+         `${url}/${user}?client_id=${cliente_id}&client_secret=${cliente_secreto}`
+      )
 
-  const ul = document.querySelector('ul')
-
-  function getApiGitHub() 
-{
-  fetch(url)
-  .then(async res => 
-  {  
-    if(!res.ok) 
-    {
-      throw new Error(res.status)
-    }
-    ////retornar uma promessa
-    const data = await res.json()
-    
-    data.map(item => 
-    {
-      let lista = document.createElement('li')
-            let aElemento = document.createElement('a')
+      const repositorioResposta = await fetch
+      (
+         `${url}/${user}/repos?per_page=${contar}&sort=${ordenar}&client_id=${cliente_id}&client_secret=${cliente_secreto}`
           
-      lista.innerHTML = ` 
-     
-        <strong>${item.name.toUpperCase()}</strong>
-        <br>
-        <br>
-        <span>${item.url}</span>
-        <br>
-        <br>
-        <span>Data Criação: 
-        ${Intl.DateTimeFormat('pt-BR')
-        .format(new Date(item.created_at))}
-        </span>
-          `
-          ul.appendChild(lista)
-    })
-    
-  }).catch(evento => console.log(evento))
-}
-    
-   
-   
+      )
+      const perfil = await perfilResposta.json()
+      const repositorio = await repositorioResposta.json()
+      
+      return {profile: perfil, repos: repositorio}
+  }
 
+  function mostrarPerfil(user) 
+  {
+      perfil.innerHTML = 
+      `
+              <img src="${user.avatar_url}" class="imagem_usuario"></div>
+              
+              <div class="ver_perfil">
+                  <a href="${user.html_url}" target="_blank">Ir para o Perfil</a>
+              </div>
+          
+              <div id="repos"></div>
+      `
+  }
+
+  function mostraRepositorio(repos) 
+  {
+      let resultado = ''
+
+      repos.forEach(repo => 
+        {
+          resultado += 
+          ` 
+            <div class="lista_repositorio">
+              <ul>
+                <li>
+                    <a href="${repo.html_url}" target="_black">${repo.name.toUpperCase()}</a></div>
+                </li>  
+              </ul>   
+            <div>                
+          `
+        })
+
+      document.getElementById("repos").innerHTML = resultado
+  }
+
+  procurar.addEventListener("keyup", evento => {
+      const user = evento.target.value
+
+      if (user.length > 0) 
+      {
+          obterUsuario(user).then(res => {
+              mostrarPerfil(res.profile)
+              mostraRepositorio(res.repos)
+          })
+      }else
+      {
+        alert('Por favor, digite o nome do usuário!')
+      }
+
+  })
+}
+pesquisar()
