9d3c766852a5067eba60d1de8188d73cc09a69f3
franciele Franco <franciele.alves1108@gmail.com>
2022-09-28 12:57:53 -0300

    Separação de responsabilidades, utilização do map,

--- docs/franciele.franco/Modulo_1/script.js
+++ docs/franciele.franco/Modulo_1/script.js
@@ -1,33 +1,36 @@
 function pesquisar() 
-{
-  const procurar = document.getElementById("usuario")
-  const perfil = document.getElementById("perfil")
+{ const perfil_usuario = document.getElementById("perfil_usuario")
   const url = "https://api.github.com/users"
   const cliente_id = "Iv1.f64f0476d5d303dd"
   const cliente_secreto = "e3e98fc69544d6b5173169f3734909907a0a3ea2"    
   const contar = 10
   const ordenar = "created: ascs"
 
-  async function obterUsuario(user) {
+  async function obterUsuario(user) 
+  {
       const perfilResposta = await fetch
       (
          `${url}/${user}?client_id=${cliente_id}&client_secret=${cliente_secreto}`
       )
-
-      const repositorioResposta = await fetch
-      (
-         `${url}/${user}/repos?per_page=${contar}&sort=${ordenar}&client_id=${cliente_id}&client_secret=${cliente_secreto}`
-          
-      )
       const perfil = await perfilResposta.json()
-      const repositorio = await repositorioResposta.json()
       
-      return {profile: perfil, repos: repositorio}
+      return {perfil}
+  }
+
+  async function obterRepositorio(user) 
+  {
+    const repositorioResposta = await fetch
+    (
+       `${url}/${user}/repos?per_page=${contar}&sort=${ordenar}&client_id=${cliente_id}&client_secret=${cliente_secreto}`   
+    )
+    const repositorio = await repositorioResposta.json()
+    
+    return {repositorio}
   }
 
   function mostrarPerfil(user) 
   {
-      perfil.innerHTML = 
+      perfil_usuario.innerHTML = 
       `
               <img src="${user.avatar_url}" class="imagem_usuario"></div>
               
@@ -35,45 +38,56 @@ function pesquisar()
                   <a href="${user.html_url}" target="_blank">Ir para o Perfil</a>
               </div>
           
-              <div id="repos"></div>
+              <div id="repos_usuario"></div>
       `
   }
 
   function mostraRepositorio(repos) 
-  {
-      let resultado = ''
+  { 
+    let resultado = repos.map(repo =>
+    {
+      return  ` 
+        <div class="lista_repositorio">
+          <ul>
+            <li>
+                <a href="${repo.html_url}" target="_black">${repo.name.toUpperCase()}</a></div>
+            </li>  
+          </ul>   
+        <div>                
+      `
+    }).join('')
 
-      repos.forEach(repo => 
-        {
-          resultado += 
-          ` 
-            <div class="lista_repositorio">
-              <ul>
-                <li>
-                    <a href="${repo.html_url}" target="_black">${repo.name.toUpperCase()}</a></div>
-                </li>  
-              </ul>   
-            <div>                
-          `
-        })
-
-      document.getElementById("repos").innerHTML = resultado
+     document.getElementById("repos_usuario").innerHTML = resultado
   }
 
-  procurar.addEventListener("keyup", evento => {
-      const user = evento.target.value
+const botao = document.getElementById("botao")
 
-      if (user.length > 0) 
+botao.addEventListener('click', event => 
+{ 
+  const user = document.getElementById("usuario").value
+
+  if (user.length > 3) 
+    { event.preventDefault()
+        obterUsuario(user).then(res => 
       {
-          obterUsuario(user).then(res => {
-              mostrarPerfil(res.profile)
-              mostraRepositorio(res.repos)
-          })
-      }else
-      {
-        alert('Por favor, digite o nome do usuário!')
+              mostrarPerfil(res.perfil)    
+      })
+
+      if(user.length > 3)
+      { 
+        obterRepositorio(user).then(res =>
+        {
+          mostraRepositorio(res.repositorio) 
+        })
       }
-
+    }
+  else
+  {
+    alert('Por favor, digite o nome do usuário!')
+    //console.log(event.target)
+  }
   })
 }
 pesquisar()
+
+
